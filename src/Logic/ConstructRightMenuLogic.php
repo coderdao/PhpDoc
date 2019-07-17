b6b15d71d8c95d78150f3ff026f0a731b55138b2
abo_o <abo2013@foxmail.com>
2019-07-17 22:40:20 +0800

    debug

---
+++ src/Logic/ConstructRightMenuLogic.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Function: 构建右侧导航栏
+ * Description:
+ * Abo 2019/7/17 21:41
+ * Email: abo2013@foxmail.com
+ */
+
+namespace Abo\Phpdoc\Logic;
+
+
+class ConstructRightMenuLogic
+{
+    private $data, $tableHtml = ''; // 每个API的信息 由parse返回的
+
+    public function __construct( array $paseData )
+    {
+        $this->data = $paseData;
+    }
+
+    public function construct()
+    {
+        $this->constructTitle();
+
+        $this->constructArray( 'param', '参数' );
+        $this->constructArray( 'return', '返回' );
+        $this->constructArray( 'throws', '异常' );
+
+        return $this->tableHtml.'<hr></div>';
+    }
+
+    /**
+     * 构建头部
+     * @return string
+     */
+    private function constructTitle()
+    {
+        $this->tableHtml = '<div id="'.base64_encode( $this->data['requestUrl']).'" class="api-main">
+        <div class="title">'. $this->data['methodName'].'</div>
+        <div class="body">
+            <table class="layui-table">
+                <thead>
+                    <tr>
+                        <th> '. $this->data['requestName'].' </th>
+                        <th rowspan="3"> '. $this->data['requestUrl'].' </th>
+                    </tr>
+                </thead>
+            </table>
+        </div>';
+
+        return $this->tableHtml;
+    }
+
+    /**
+     * 构建 数组内容: 请求参数/返回
+     * @return string
+     */
+    private function constructArray( string $constructArrayKey, string $label )
+    {
+        if ( !$constructArrayKey ) { return $this->tableHtml; }
+
+        // 请求参数
+        $data = $this->data;
+        if( !isset( $data[ $constructArrayKey ] ) || !$data[ $constructArrayKey ] ){ return $this->tableHtml; }
+
+        $this->tableHtml .= '<div class="body">
+                <table class="layui-table">
+                    <thead>
+                        <tr> 
+                            <th>'.$label.'名称</th> 
+                            <th>'.$label.'类型</th> 
+                            <th>'.$label.'说明</th> 
+                        </tr>
+                    </thead>
+                    <tbody>';
+
+        foreach( $data[ $constructArrayKey ] as $param ){
+            $this->tableHtml .= "<tr> 
+                    <td>{$param['var']}</td> 
+                    <td>{$param['type']}</td> 
+                    <td>{$param['about']}</td>
+                </tr>";
+        }
+
+        $this->tableHtml .= '</tbody>
+                </table>
+            </div>';
+
+        return $this->tableHtml;
+    }
+
+
+}
